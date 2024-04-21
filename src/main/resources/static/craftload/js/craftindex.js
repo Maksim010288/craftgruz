a44a9af30262155d68c7d6bce42e7c347f4aae50
Maksym <mysik.bazyk@gmail.com>
2024-04-21 16:10:43 +0300

    refactoring js file

--- src/main/resources/static/craftload/js/craftindex.js
+++ src/main/resources/static/craftload/js/craftindex.js
@@ -1,47 +1,53 @@
 let confirmation_of_shipment = document.querySelectorAll('.messageTop, .messageButtom')
 let calculateButtom = document.querySelectorAll(".calculateCostButtom");
+let numberPhone = document.querySelectorAll('.numberPhone');
 
+for (let i = 0; i < calculateButtom.length; i++) {
+    calculateButtom[i].setAttribute('disabled', true);
+    calculateButtom[i].style.background = "grey";
+}
 
-calculateButtom[0].addEventListener("click", ()=>{
+for (let i = 0; i < numberPhone.length; i++) {
+    numberPhone[i].oninput = function () {
+        if (numberPhone[0].value.length !== 0 &&
+            numberPhone[1].value.length !== 0 &&
+            numberPhone[2].value.length !== 0 &&
+            numberPhone[3].value.length !== 0 &&
+            numberPhone[4].value.length !== 0 &&
+            numberPhone[5].value.length !== 0) {
+            calculateButtom[0].removeAttribute('disabled');
+            calculateButtom[0].style.background = 'darkcyan';
+        } else {
+            calculateButtom[0].setAttribute('disabled', true);
+            calculateButtom[0].style.background = 'grey';
+        }
+
+        if (numberPhone[6].value.length !== 0 &&
+            numberPhone[7].value.length !== 0 &&
+            numberPhone[8].value.length !== 0 &&
+            numberPhone[9].value.length !== 0 &&
+            numberPhone[10].value.length !== 0 &&
+            numberPhone[11].value.length !== 0) {
+            calculateButtom[1].removeAttribute('disabled');
+            calculateButtom[1].style.background = 'darkcyan';
+        } else {
+            calculateButtom[1].setAttribute('disabled', true);
+            calculateButtom[1].style.background = 'grey';
+        }
+    }
+}
+
+calculateButtom[0].addEventListener("click", () => {
     confirmation_of_shipment[0].innerHTML = "Повідомлення відправлено!!!"
-    setTimeout(()=>{
+    setTimeout(() => {
         confirmation_of_shipment[0].innerHTML = " "
     }, 2000)
 })
 
-calculateButtom[1].addEventListener("click", ()=>{
+calculateButtom[1].addEventListener("click", () => {
     confirmation_of_shipment[1].innerHTML = "Повідомлення відправлено!!!"
-    setTimeout(()=>{
+    setTimeout(() => {
         confirmation_of_shipment[1].innerHTML = " "
     }, 2000)
 })
 
-
-let butForm = document.querySelectorAll(".calculateCostButtom")
-for(let i = 0; i < butForm.length; i++) {
-    butForm[i].setAttribute('disabled', true);
-    butForm[i].style.background = "grey";
-}
-let numberPhone = document.querySelectorAll('.numberPhone');
-
-for(let i = 0; i < numberPhone.length; i++){
-    numberPhone[i].oninput = function(){
-        if(numberPhone[0].value.length !== 0 && numberPhone[1].value.length !== 0 &&
-            numberPhone[2].value.length !== 0 && numberPhone[3].value.length !== 0 &&
-            numberPhone[4].value.length !== 0 && numberPhone[5].value.length !== 0){
-            butForm[0].removeAttribute('disabled');
-            butForm[0].style.background = 'darkcyan';
-        } else {
-            butForm[0].setAttribute('disabled', true);
-        }
-
-        if(numberPhone[6].value.length !== 0 && numberPhone[7].value.length !== 0 &&
-            numberPhone[8].value.length !== 0 && numberPhone[9].value.length !== 0 &&
-            numberPhone[10].value.length !== 0 && numberPhone[11].value.length !== 0){
-            butForm[1].removeAttribute('disabled');
-            butForm[1].style.background = 'darkcyan';
-        } else {
-            butForm[1].setAttribute('disabled', true);
-        }
-    }
-}
